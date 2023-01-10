00fca44300916c0d4a4a8fdf897311a9961e8730
Anna <tka4evaanna2002@yandex.ru>
2023-01-11 00:08:14 +0300

    task5

--- task5.py
+++ task5.py
@@ -2,36 +2,18 @@
 # Пример:
 # для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] Негафибоначчи
 
-# def fin (n):
-#     if n in [1,2]:
-#         return 1
-#     else:
-#         return fin(n-1) + fin(n-2)
         
 a = int(input('Введите число: '))
 
-fib_res = []
-fib2_list = []
-fib_list = [0,1]
+fib_list= [0,1]
 
 
-for i in range(a-1):
-    fib_list[i]= fib_list[i-2] + fib_list[i-1] 
-    fib_list.append(fib_list[i])
-    # fib2_list[i] = fib2_list[i+1]- fib2_list[i+2] 
-    # fib2_list.append(fib2_list[i])
+for i in range(a):
+    fib = fib_list[-2] + fib_list[-1] 
+    fib_list.append(fib)
+    fib2 = fib_list[1]-fib_list[0]
+    fib_list.insert(0,fib2)
 
-print(fib_list)  
-# print(fib2_list)
+fib_list = fib_list[:-1]
+print(fib_list)
 
-# for item in range ()
-# fib2_list = fib_list.reverse()
-# print (fib2_list)
-
-
-
-# fib_list1.reverse()
-# res_list = fib_list1.copy()
-# res_list.extend(fib_list)
-
-# print(f'Для числа {a} последовательность Фибоначчи и Негафибоначчи: {res_list}')
